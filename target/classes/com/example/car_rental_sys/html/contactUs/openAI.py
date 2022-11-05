9f65ae584c1874a227d22f20de700f3dce243dc2
Innis <1575270674@qq.com>
2022-11-06 03:42:27 +0800

    add openai backend file

---
+++ target/classes/com/example/car_rental_sys/html/contactUs/openAI.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from flask_cors import CORS
+from flask import Flask, request
+import openai
+
+
+app = Flask(__name__)
+CORS(app, resources=r'/*')
+
+
+@app.route('/response', methods=["POST"])
+def postExplainToCache():
+    jsonData = request.json
+    sentence = jsonData["sentences"]
+    return chat(sentence)
+
+
+
+def chat(messageList):
+    openai.api_key ="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"
+
+    start_sequence = "\nAI:"
+    restart_sequence = "\nHuman: "
+
+    message = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly car rental system customer service robot."
+
+
+    i = 1
+    for ele in messageList:
+        if i % 2 == 0:
+            message += start_sequence + ele
+        else:
+            message += restart_sequence + ele
+        i += 1
+
+
+    response = openai.Completion.create(
+        model="text-curie-001",
+        # model="text-davinci-002",
+        prompt=message,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )![](C:/Users/Innis/AppData/Local/Temp/download.jpg)
+    response = response["choices"][0]["text"].replace(
+        "\nAI:", "").replace("\n", "")
+
+    return {"response": response}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='10.0.24.5', port="81")
