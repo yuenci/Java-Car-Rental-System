656a96ed466fef4d35d2173ae2e89cab56ebafce
Innis <1575270674@qq.com>
2022-11-07 18:29:33 +0800

    test: import fxml to pane

--- src/main/java/com/example/car_rental_sys/Application.java
+++ src/main/java/com/example/car_rental_sys/Application.java
@@ -17,20 +17,21 @@ public class Application extends javafx.application.Application {
     @Override
     public void init() throws Exception {
         registerJxBrowserLicence();
-        StartHttpServer();
+        //StartHttpServer();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
        // String fxmlName = "mainPage.fxml";
-        //String fxmlName = "carsListPage.fxml";
+        String fxmlName = "carsListPage.fxml";
         //String fxmlName = "signUpPage.fxml";
-        String fxmlName = "loginPage.fxml";
+        //String fxmlName = "loginPage.fxml";
         //String fxmlName = "carDetailsPage.fxml";
         //String fxmlName = "paymentPage.fxml";
         //String fxmlName = "contactUsPage.fxml";
         //String fxmlName = "OrderDetailsComponent.fxml";
         //String fxmlName = "paySuccessPage.fxml";
+        //String fxmlName = "aboutUsPage.fxml";
 
         stageInstance = stage;
 
