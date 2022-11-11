f9582670747b99896dc2dfabadafbd6133d10a67
Innis <1575270674@qq.com>
2022-11-11 19:22:28 +0800

    Feat: add property View

--- src/main/java/com/example/car_rental_sys/orm/User.java
+++ src/main/java/com/example/car_rental_sys/orm/User.java
@@ -1,5 +1,25 @@
 package com.example.car_rental_sys.orm;
 
 public class User {
+    private int userID;
+    private String userName;
+    private String userGroup;
+    private String post;
+    private String securityProblem;
+    private String securityAnswer;
+    private String birthday;
+    private String gender;
+    private String country;
+    private String DLNumber;
+    private String phone;
+    private String email;
+    private String address;
+    private String about;
+    private String regTime;
+
+    public User(String email) {
+        this.email = email;
+
+    }
 
 }
