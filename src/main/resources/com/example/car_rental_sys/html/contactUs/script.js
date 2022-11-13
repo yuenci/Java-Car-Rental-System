61338ec4c164b274bddae36c0509abeedc5630ce
Innis <1575270674@qq.com>
2022-11-13 16:43:45 +0800

    to: refactor contact

--- src/main/resources/com/example/car_rental_sys/html/contactUs/script.js
+++ src/main/resources/com/example/car_rental_sys/html/contactUs/script.js
@@ -28,13 +28,25 @@ class Tools {
     }
 
     static cleanResponse(response) {
-        if (response[0] === ",") response = response.slice(1);
+        let charater = [",", "!", "?", ".", ":", ";"];
+        if (charater.includes(response[0])) response = response.slice(1);
 
-        nowords = [".Human: "]
+
+        let nowords = [".Human: "]
         for (let i = 0; i < nowords.length; i++) {
             response = response.replace(nowords[i], "");
         }
+        return response;
 
+    }
+
+    static getMessageList() {
+        let res = [];
+        let messageList = document.getElementsByClassName("message");
+        for (let i = 0; i < messageList.length; i++) {
+            res.push(messageList[i].innerHTML);
+        }
+        return res;
     }
 }
 
@@ -56,6 +68,7 @@ class Message {
     }
 
     addServerMessage() {
+        this.message = Tools.cleanResponse(this.message);
         let messageRes = `
                         <div class="message-container-left">
                             <div class="avatar">
@@ -91,66 +104,59 @@ class Message {
 
 }
 
+class OpenAI {
+    static async sendMessage() {
+        let message = $inputBox.val();
+        if (message.length === 0) {
+            return;
+        }
+        //// addCustomerMessage(message);
+        let newMessage = new Message(message, "me", true);
+        $inputBox.val("");
+        scrollToBottom();
 
-// let servicers = ["Nigel Walsh",
-//     "Kirk Gallacher",
-//     "Blair Christopher",
-//     "Gerald Buckle",
-//     "Boyd Chamberlain",
-//     "Louis Gresham",
-//     "Marcia Mary",
-//     "Janet Wordsworth",
-//     "Yedda Dierser",
-//     "Veromca Harry",
-//     "Vanessa Lambert",
-//     "Olga Nick"]
-// let index = Math.round(Math.random() * (11));
-// let servicer = servicers[index];
+        await OpenAI.getResponse().then(function (data) {
+            //// addServerMessage(data["response"]);
 
-// let message = "hihiiiiiiii"
+            let newMessage = new Message(data["response"], servicer, false);
+        }).catch(function (error) {
+            OpenAI.netWorkError();
+        });
+    }
+
+    static getResponse() {
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                type: 'POST',
+                url: `http://101.43.138.40:81/response`,
+                data: JSON.stringify({ "sentences": Tools.getMessageList() }),
+                success: function (data) {
+                    console.log(data);
+                    resolve(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(errorThrown);
+                },
+                contentType: "application/json",
+                dataType: 'json'
+            });
+        });
+    }
+
+    static netWorkError() {
+        let message = "Network error, please try again later";
+        let messageRes = `
+        <div class="error-message-container"><p class="error-message">${message}</p></div>
+    `
+        $("#char-area-bottom").before(messageRes);
+    }
+}
+
+
+
 let servicer = Servicer.getRandomServicer();
 let welcomeMessage = new Message(`Hi, welcome to Rent, I'm ${servicer}, how can I help you?`, servicer, false);
 
-// function addServerMessage(message) {
-//     let messageRes = `
-//     <div class="message-container-left">
-//         <div class="avatar">
-//             <img src="./logo.png" alt="Logo" class="logoImage">
-//         </div>
-//         <div class="message-area">
-//             <div class="message-topic">
-//                 <div class="message-sender">${servicer}</div>
-//                 <div class="message-time">${getTime()}</div>
-//             </div>
-//             <div class="message">${message}</div>
-//         </div>
-//     </div>
-//     `
-//     $("#char-area-bottom").before(messageRes);
-//     scrollToBottom();
-// }
-
-// function getTime() {
-//     let newDate = new Date();
-//     let format = (x) => x.toString().padStart(2, "0");
-//     let time = format(newDate.getHours()) + ":" + format(newDate.getMinutes()) + ":" + format(newDate.getSeconds());
-//     return time;
-// }
-
-// function addCustomerMessage(message) {
-
-//     let messageRes = `
-//     <div class="message-container-right">
-//         <div class="message-area">
-//             <div class="message-topic">
-//                 <div class="message-time">${getTime()}</div>
-//             </div>
-//             <div class="message">${message}</div>
-//         </div>
-//     </div>
-//     `
-//     $("#char-area-bottom").before(messageRes);
-// }
 
 
 $("#sendBtn").click(function () {
@@ -161,70 +167,72 @@ let $inputBox = $("#inputBox");
 
 $inputBox.keypress(function (e) {
     if (e.which === 13) {
-        sendMessage();
+        OpenAI.sendMessage();
     }
 });
 
-async function sendMessage() {
-    let message = $inputBox.val();
-    if (message.length === 0) {
-        return;
-    }
-    //// addCustomerMessage(message);
-    let newMessage = new Message(message, "me", true);
-    $inputBox.val("");
-    scrollToBottom();
+// async function sendMessage() {
+//     let message = $inputBox.val();
+//     if (message.length === 0) {
+//         return;
+//     }
+//     //// addCustomerMessage(message);
+//     let newMessage = new Message(message, "me", true);
+//     $inputBox.val("");
+//     scrollToBottom();
 
-    await getResponse().then(function (data) {
-        //// addServerMessage(data["response"]);
+//     await getResponse().then(function (data) {
+//         //// addServerMessage(data["response"]);
 
-        let newMessage = new Message(data["response"], servicer, false);
-    }).catch(function (error) {
-        netWorkError();
-    });
+//         let newMessage = new Message(data["response"], servicer, false);
+//     }).catch(function (error) {
+//         netWorkError();
+//     });
 
-}
+// }
 
-function getResponse() {
-    return new Promise(function (resolve, reject) {
-        $.ajax({
-            type: 'POST',
-            url: `http://101.43.138.40:81/response`,
-            data: JSON.stringify({ "sentences": getMessageList() }),
-            success: function (data) {
-                console.log(data);
-                resolve(data);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                reject(errorThrown);
-            },
-            contentType: "application/json",
-            dataType: 'json'
-        });
-    });
-}
+// function getResponse() {
+//     return new Promise(function (resolve, reject) {
+//         $.ajax({
+//             type: 'POST',
+//             url: `http://101.43.138.40:81/response`,
+//             data: JSON.stringify({ "sentences": getMessageList() }),
+//             success: function (data) {
+//                 console.log(data);
+//                 resolve(data);
+//             },
+//             error: function (jqXHR, textStatus, errorThrown) {
+//                 reject(errorThrown);
+//             },
+//             contentType: "application/json",
+//             dataType: 'json'
+//         });
+//     });
+// }
 
-function getMessageList() {
-    let res = [];
-    let messageList = document.getElementsByClassName("message");
-    for (let i = 0; i < messageList.length; i++) {
-        res.push(messageList[i].innerHTML);
-    }
-    return res;
-}
+// function getMessageList() {
+//     let res = [];
+//     let messageList = document.getElementsByClassName("message");
+//     for (let i = 0; i < messageList.length; i++) {
+//         res.push(messageList[i].innerHTML);
+//     }
+//     return res;
+// }
 
-function heightToTop(ele) {
-    //ele为指定跳转到该位置的DOM节点
-    let root = document.body;
-    let height = 0;
-    do {
-        height += ele.offsetTop;
-        ele = ele.offsetParent;
-    } while (ele !== root)
-    return height;
-}
+
 
 function scrollToBottom() {
+    function heightToTop(ele) {
+        //ele为指定跳转到该位置的DOM节点
+        let root = document.body;
+        let height = 0;
+        do {
+            height += ele.offsetTop;
+            ele = ele.offsetParent;
+        } while (ele !== root)
+        return height;
+    }
+
     let bottom = document.getElementById("char-area-bottom");
     let chatArea = document.getElementById("chat-area");
     chatArea.scrollTo({
@@ -242,10 +250,10 @@ $inputBox.focus(function () {
     container.classList.remove("comtainer-active");
 });
 
-function netWorkError() {
-    let message = "Network error, please try again later";
-    let messageRes = `
-        <div class="error-message-container"><p class="error-message">${message}</p></div>
-    `
-    $("#char-area-bottom").before(messageRes);
-}
+// function netWorkError() {
+//     let message = "Network error, please try again later";
+//     let messageRes = `
+//         <div class="error-message-container"><p class="error-message">${message}</p></div>
+//     `
+//     $("#char-area-bottom").before(messageRes);
+// }
