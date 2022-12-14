0300e68e0120a7bfbba388899d7a0d88ccae6fbd
Kaikiat <kaikiat@gmail.com>
2022-12-14 16:13:48 +0800

    feat: invoice drag and drop function fix: invoice close button not response

--- src/main/java/com/example/car_rental_sys/ui_components/InvoiceBox.java
+++ src/main/java/com/example/car_rental_sys/ui_components/InvoiceBox.java
@@ -6,25 +6,34 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class InvoiceBox {
 
+    public static InvoiceBox instance;
+    public Stage stageInvoice = null;
+
     public InvoiceBox() {
         //DO NOTHING
+        instance = this;
     }
 
     public void showInvoiceStage() throws IOException {
-        Stage stageInvoice = new Stage();
+        stageInvoice = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("fxml/Invoice.fxml"));
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         stageInvoice.setX((screenBounds.getWidth() - 500) / 2);
         stageInvoice.setY((screenBounds.getHeight() - 600) / 2);
         Scene scene = new Scene(fxmlLoader.load(), 500, 600);
-        //stageInvoice.initStyle(StageStyle.UNDECORATED);
-        stageInvoice.setTitle("Test Invoice");
+        stageInvoice.initStyle(StageStyle.UNDECORATED);
+        stageInvoice.setTitle("Order Invoice");
         stageInvoice.setScene(scene);
         stageInvoice.show();
+    }
+
+    public void closeInvoiceStage() throws IOException {
+        stageInvoice.close();
     }
 }
